805a57a5bacb7d388feda3e38fa17f56ce7daa7d
BinarySemaphore <buetowbp@gmail.com>
2023-06-15 03:11:31 -0700

    Updated docs.

--- Doxygen/html/search/all_9.js
+++ Doxygen/html/search/all_9.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['largest_0',['largest',['../a00029.html#a905a383c4185a0f82472fe6a09558b5faf39ed5b6d0275dcbc4a2f5de624d95ae',1,'AABB']]],
   ['last_5fpostion_1',['last_postion',['../a00037.html#aa216568efddbd22ad71c2ec4b15e9bde',1,'Body']]],
-  ['last_5frotation_2',['last_rotation',['../a00037.html#a775cba97ccfe8919965afc24169913d8',1,'Body']]],
+  ['last_5frotation_2',['last_rotation',['../a00037.html#a338fbde8c1b715f7b9adda41e34debaa',1,'Body']]],
   ['location_3',['location',['../a00041.html#a5922b19abd48805b3d893ecbe5946a96',1,'Collision']]]
 ];
--- Doxygen/html/search/functions_b.js
+++ Doxygen/html/search/functions_b.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['updateaccumulations_0',['UpdateAccumulations',['../a00037.html#ae554f6a91dd049cf9838f7902df52959',1,'Body']]],
   ['updateforcollisions_1',['UpdateForCollisions',['../a00037.html#a9f6291e5c166a4e73b183102e25312af',1,'Body']]],
-  ['updatepositions_2',['UpdatePositions',['../a00037.html#afbd34131156fb6411e663a5e33c6a6b7',1,'Body']]],
-  ['updatesim_3',['UpdateSim',['../a00061.html#a551c9b517e70a1ae8a079a09a78b04fc',1,'Simulator']]]
+  ['updateiteration_2',['UpdateIteration',['../a00061.html#ac891571589c40d7801e46b49e99b75e0',1,'Simulator']]],
+  ['updatepositions_3',['UpdatePositions',['../a00037.html#afbd34131156fb6411e663a5e33c6a6b7',1,'Body']]],
+  ['updatesim_4',['UpdateSim',['../a00061.html#ad15a684feaa4ef4cac9e8ee4e83aaa91',1,'Simulator']]]
 ];
